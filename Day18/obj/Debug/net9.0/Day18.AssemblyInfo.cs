a2e01dac874d0e8e70409cd33e3f575d77beaed7
Ritz55555 <rua6376@gmail.com>
2026-05-18 14:28:19 +0900

    Path_Changes

--- Day18/obj/Debug/net9.0/Day18.AssemblyInfo.cs
+++ Day18/obj/Debug/net9.0/Day18.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Day18")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9c5d90a307b9402835cd52b876b11521c589f616")]
 [assembly: System.Reflection.AssemblyProductAttribute("Day18")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Day18")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
